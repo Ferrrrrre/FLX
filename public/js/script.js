0db18a060c37dd2aa7c7199cb3a42b48c62babe1
Ferre <ferrelambert@hotmail.com>
2017-07-30 23:22:22 +0200

    bugfix isotope

--- public/js/script.js
+++ public/js/script.js
@@ -4,6 +4,9 @@ $(function(){
 	var grid = $('.grid').isotope({
 		  // options
 		  itemSelector: '.item-container',
+          masonry: {
+
+          },
           sortBy: 'random'
 	});
 
@@ -12,36 +15,6 @@ $(function(){
         grid.isotope({ filter: filterValue });
         console.log($("." + filterValue));
     });
-
-	var depth = 100;
-	
-	// element to detect scroll direction of
-	var el = $(window),
-
-    // initialize last scroll position
-    lastY = el.scrollTop();
-    
-	el.on('scroll', function() {
-		console.log('scrolling');
-    // get current scroll position
-    var currY = el.scrollTop();
-        if(currY > lastY){
-        	depth++;
-        }
-        else if(currY === lastY){
-        	
-        }
-        else{
-        	depth--;
-        }
-
-        // determine current scroll direction
-    //$('.background').css('background-size', depth + '%');
-    
-
-    // update last scroll position to current position
-    lastY = currY;
-	});
 
 });
 
